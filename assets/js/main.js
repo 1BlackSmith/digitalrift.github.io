2c0e335d61d8891dc03a5735d92077bd8290279f
geooooo <geo97it@mail.com>
2018-06-21 13:59:32 +0300

    add little feedback block

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,7 @@ window.addEventListener('load', function(e) {
         blockLoading.style.display = "none";
     }, 500);
 
+
     /* Переключение флажка */
 
     var checkboxs = document.getElementsByClassName('checkbox');
@@ -18,30 +19,42 @@ window.addEventListener('load', function(e) {
         });
     }
 
-    /* Закрытие блока отправки заявки */
-
-    var blockFeedback = document.getElementById("blockFeedback");
-    var closeBlockFeedback = document.getElementById("closeBlockFeedback");
-    closeBlockFeedback.addEventListener("click", function(e) {
-        blockFeedback.classList.add("block-feedback_hide");
-        window.setTimeout(function() {
-            blockFeedback.style.display = "none";
-            blockFeedback.classList.remove("block-feedback_hide");
-            var checkbox = blockFeedback.getElementsByClassName("checkbox")[0];
-            checkbox.classList.remove('checkbox_active');
-        }, 400);
-    });
 
     /* Открытие блока отправки заявки */
 
-     var buttons = document.getElementsByClassName("blockFeedbackShow");
-     for (var i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener("click", function(e) {
-            blockFeedback.style.display = "flex";
-            blockFeedback.classList.add("block-feedback_show");
+    var blockFeedbackBig = document.getElementById("blockFeedbackBig");
+    var blockFeedbackLittle = document.getElementById("blockFeedbackLittle");
+    var buttonsShowLittle = document.getElementsByClassName("blockFeedbackShowLittle");
+    var buttonsShowBig = document.getElementsByClassName("blockFeedbackShowBig");
+    bind(blockFeedbackBig, buttonsShowBig);
+    bind(blockFeedbackLittle, buttonsShowLittle);
+
+    function bind(block, buttons) {
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].addEventListener("click", function(e) {
+                block.style.display = "flex";
+                block.classList.add("block-feedback_show");
+                window.setTimeout(function() {
+                    block.classList.remove("block-feedback_show");
+                    block.style.opacity = 1;
+                }, 400);
+            });
+         }
+    }
+
+
+    /* Закрытие блока отправки заявки */
+
+    var blockFeedbacks = document.getElementsByClassName("block-feedback");
+    for (let i = 0; i < blockFeedbacks.length; i++) {
+        var closeButton = blockFeedbacks[i].getElementsByClassName("block-feedback__button-close")[0];
+        closeButton.addEventListener("click", function(e) {
+            blockFeedbacks[i].classList.add("block-feedback_hide");
             window.setTimeout(function() {
-                blockFeedback.classList.remove("block-feedback_show");
-                blockFeedback.style.opacity = 1;
+                blockFeedbacks[i].style.display = "none";
+                blockFeedbacks[i].classList.remove("block-feedback_hide");
+                var checkbox = blockFeedbacks[i].getElementsByClassName("checkbox")[0];
+                checkbox.classList.remove('checkbox_active');
             }, 400);
         });
     }
