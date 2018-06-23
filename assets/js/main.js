05cbb0765513d006069b14739bb0e80022e2c75b
Pavel BlackSmith <1BlackSmith.work@gmail.com>
2018-06-23 19:22:29 +0300

    change bg-images and add waypoints animation (example)

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,10 @@ window.addEventListener('load', function(e) {
 
     /* Плавное исчезновение блока загрузки */
 
-    var blockLoading = document.getElementById("blockLoading");
-    blockLoading.classList.add("block-loading_hide");
+    var blockLoading = document.getElementById('blockLoading');
+    blockLoading.classList.add('block-loading_hide');
     window.setTimeout(function() {
-        blockLoading.style.display = "none";
+        blockLoading.style.display = 'none';
     }, 500);
 
 
@@ -21,20 +21,20 @@ window.addEventListener('load', function(e) {
 
     /* Открытие блока отправки заявки */
 
-    var blockFeedbackBig = document.getElementById("blockFeedbackBig");
-    var blockFeedbackLittle = document.getElementById("blockFeedbackLittle");
-    var buttonsShowLittle = document.getElementsByClassName("blockFeedbackShowLittle");
-    var buttonsShowBig = document.getElementsByClassName("blockFeedbackShowBig");
+    var blockFeedbackBig = document.getElementById('blockFeedbackBig');
+    var blockFeedbackLittle = document.getElementById('blockFeedbackLittle');
+    var buttonsShowLittle = document.getElementsByClassName('blockFeedbackShowLittle');
+    var buttonsShowBig = document.getElementsByClassName('blockFeedbackShowBig');
     bind(blockFeedbackBig, buttonsShowBig);
     bind(blockFeedbackLittle, buttonsShowLittle);
 
     function bind(block, buttons) {
         for (var i = 0; i < buttons.length; i++) {
-            buttons[i].addEventListener("click", function(e) {
-                block.style.display = "flex";
-                block.classList.add("block-feedback_show");
+            buttons[i].addEventListener('click', function(e) {
+                block.style.display = 'flex';
+                block.classList.add('block-feedback_show');
                 window.setTimeout(function() {
-                    block.classList.remove("block-feedback_show");
+                    block.classList.remove('block-feedback_show');
                     block.style.opacity = 1;
                 }, 400);
             });
@@ -44,17 +44,31 @@ window.addEventListener('load', function(e) {
 
     /* Закрытие блока отправки заявки */
 
-    var blockFeedbacks = document.getElementsByClassName("block-feedback");
+    var blockFeedbacks = document.getElementsByClassName('block-feedback');
     for (let i = 0; i < blockFeedbacks.length; i++) {
-        var closeButton = blockFeedbacks[i].getElementsByClassName("block-feedback__button-close")[0];
-        closeButton.addEventListener("click", function(e) {
-            blockFeedbacks[i].classList.add("block-feedback_hide");
+        var closeButton = blockFeedbacks[i].getElementsByClassName('block-feedback__button-close')[0];
+        closeButton.addEventListener('click', function(e) {
+            blockFeedbacks[i].classList.add('block-feedback_hide');
             window.setTimeout(function() {
-                blockFeedbacks[i].style.display = "none";
-                blockFeedbacks[i].classList.remove("block-feedback_hide");
-                var checkbox = blockFeedbacks[i].getElementsByClassName("checkbox")[0];
+                blockFeedbacks[i].style.display = 'none';
+                blockFeedbacks[i].classList.remove('block-feedback_hide');
+                var checkbox = blockFeedbacks[i].getElementsByClassName('checkbox')[0];
                 checkbox.classList.remove('checkbox_active');
             }, 400);
+        });
+    }
+
+
+    /* Появление элементов при скролле */
+
+    var waypoints = document.getElementsByClassName('waypoint');
+    for (let i = 0; i < waypoints.length; i++) {
+        new Waypoint({
+            element: waypoints[i],
+            handler: function(direction) {
+                console.log(this.element.id + ' triggers at ' + this.triggerPoint);
+            },
+            offset: '75%'
         });
     }
 
