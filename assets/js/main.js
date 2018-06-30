b50ed1af9a700e990d743a8e678e46b37a6bde60
Pavel BlackSmith <1BlackSmith.work@gmail.com>
2018-06-30 16:27:19 +0300

    change modals and forms
    add script sending data on mail
    add validation in forms
    add modal which opened after send form data

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,22 @@ window.addEventListener('load', function( e ) {
 
     /**
      * Перебор элементов DOM
-     * elems - переменная, в которую записаны элементы
-     * func - функция обработки для каждого элемента
+     * @param elems - переменная, в которую записаны элементы
+     * @param func - функция обработки для каждого элемента
      */
     function forEach( elems, func ) {
         return [].forEach.call(elems, function ( elem, i, elems ) {
+            return func.apply(elem, arguments);
+        });
+    }
+
+    /**
+     * Фильтрация элементов DOM
+     * @param elems - переменная, в которую записаны элементы
+     * @param func - функция обработки для каждого элемента
+     */
+    function filter( elems, func ) {
+        return [].filter.call(elems, function ( elem, i, elems ) {
             return func.apply(elem, arguments);
         });
     }
@@ -26,12 +37,12 @@ window.addEventListener('load', function( e ) {
     /* Открытие и закрытие модальных окон */
 
     var modals = document.getElementsByClassName('modal');
-    forEach(modals, function () {
-        var modal = this;
-        var modalId = this.getAttribute('id');
-        var togglesOpen = document.querySelectorAll('[data-modal-open="' + modalId + '"]');
-        var toggleClose = document.querySelector('[data-modal-close="' + modalId + '"]');
+    forEach(modals, function ( modal ) {
+        var modalId = this.getAttribute('id'),
+            togglesOpen = document.querySelectorAll('[data-modal-open="' + modalId + '"]'),
+            togglesClose = document.querySelectorAll('[data-modal-close="' + modalId + '"]');
 
+        // Вешаем событие на все переключатели открытия модалки
         forEach(togglesOpen, function () {
             this.addEventListener('click', function ( e ) {
                 e.preventDefault();
@@ -40,10 +51,13 @@ window.addEventListener('load', function( e ) {
             });
         });
 
-        toggleClose.addEventListener('click', function ( e ) {
-            e.preventDefault();
-            document.body.classList.remove('fixed');
-            modal.classList.remove('modal--open');
+        // Вешаем событие на все переключатели закрытия модалки
+        forEach(togglesClose, function () {
+            this.addEventListener('click', function ( e ) {
+                e.preventDefault();
+                document.body.classList.remove('fixed');
+                modal.classList.remove('modal--open');
+            });
         });
     });
 
@@ -52,7 +66,7 @@ window.addEventListener('load', function( e ) {
     /* Появление элементов при скролле */
 
     var waypoints = document.getElementsByClassName('wp');
-    for (let i = 0; i < waypoints.length; i++) {
+    forEach(waypoints, function ( elem, i) {
         new Waypoint({
             element: waypoints[i],
             handler: function(direction) {
@@ -60,7 +74,7 @@ window.addEventListener('load', function( e ) {
             },
             offset: '70%'
         });
-    }
+    });
 
 
 
@@ -70,6 +84,39 @@ window.addEventListener('load', function( e ) {
         speed: 600,
         easing: 'linear',
         updateURL: false 
+    });
+
+
+
+    /* Отправка заявки */
+
+    forEach(document.forms, function ( form ) {
+        form.addEventListener('submit', function ( e ) {
+            e.preventDefault();
+            var inputs = form.querySelectorAll('input'),
+                xhr = new XMLHttpRequest(),
+                data = 'name=' + encodeURIComponent(inputs[0].value) + '&tel=' + encodeURIComponent(inputs[1].value);
+
+            // Отправка пост запроса
+            xhr.open("POST", '/send.php?send', true);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                    if (+xhr.responseText == 1) {
+                        var modals = document.getElementsByClassName('modal');
+                        // Закрываем все модалки
+                        forEach(modals, function () {
+                            this.classList.remove('modal--open');
+                        });
+                        // Открываем модалку с сообщением
+                        var thanks = document.getElementById('thanks');
+                        document.body.classList.add('fixed');
+                        thanks.classList.add('modal--open');
+                    }
+                };
+            };
+            xhr.send(data);
+        });
     });
 
 });
---
+++ send.php
@@ -0,0 +1,30 @@
+<?php 
+
+if (isset($_GET['send'])) {
+	$name = $_POST['name'];
+	$tel = $_POST['tel'];
+
+	$to = 'digitalrift@yandex.ru';
+	$subject = '=?utf-8?B?'. base64_encode('Заявка с сайта DigitalRift') .'?=';
+	
+	$message = '
+    <html>
+      <head>
+        <title>'.$subject.'</title>
+      </head>
+      <body>
+				<p>Имя: '. $name .'</p>
+				<p>Телефон: '. $tel .'</p>
+      </body>
+    </html>';
+
+	$header = "From: ". base64_encode($name) ."\r\n";
+	$header .= "MIME-Version: 1.0\r\n";
+	$header .= "Content-type: text/html; charset=utf-8\r\n";
+	
+
+	/* Отправка письма */ 
+	$result = mail($to, $subject, $message, $header);
+
+	echo $result;
+}
