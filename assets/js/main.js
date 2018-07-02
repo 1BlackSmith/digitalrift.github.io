f0169774f02773c75eae3f6bf845a68a1cb79aa7
Pavel BlackSmith <1BlackSmith.work@gmail.com>
2018-07-02 14:21:49 +0300

    fix preloader hide animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,11 +60,13 @@ window.addEventListener('load', function( e ) {
 
     /* Плавное исчезновение блока загрузки */
 
-    var blockLoading = document.getElementById('blockLoading');
-    blockLoading.classList.add('block-loading_hide');
-    window.setTimeout(function() {
-        blockLoading.style.display = 'none';
-    }, 500);
+    setTimeout(function () {
+        var blockLoading = document.getElementById('blockLoading');
+        blockLoading.classList.add('block-loading--hide');
+        window.setTimeout(function() {
+            blockLoading.style.display = 'none';
+        }, 500);
+    }, 1000);
 
 
 
